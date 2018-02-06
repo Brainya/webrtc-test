fa62f8a82cd301bd40dcd650d6232b7f07792253
Brainya <bombird.dtb@gmail.com>
2018-02-07 04:37:28 +0900

    [add] OneSignal notification

--- ios/webrtc_test/AppDelegate.m
+++ ios/webrtc_test/AppDelegate.m
@@ -11,11 +11,29 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <OneSignal/OneSignal.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // Replace '11111111-2222-3333-4444-0123456789ab' with your OneSignal App ID.
+  [OneSignal initWithLaunchOptions:launchOptions
+                             appId:@"ff7cda9e-6192-4ffa-a36b-e48428a53ace"
+          handleNotificationAction:nil
+                          settings:@{kOSSettingsKeyAutoPrompt: @false}];
+  OneSignal.inFocusDisplayType = OSNotificationDisplayTypeNotification;
+  
+  // Recommend moving the below line to prompt for push after informing the user about
+  //   how your app will use them.
+  [OneSignal promptForPushNotificationsWithUserResponse:^(BOOL accepted) {
+    NSLog(@"User accepted notifications: %d", accepted);
+  }];
+  
+  // Call syncHashedEmail anywhere in your iOS app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // [OneSignal syncHashedEmail:userEmail];
+  
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
